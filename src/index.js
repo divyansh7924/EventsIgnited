6a97303e4419b96b31b5854f1fc0a6094247c86b
divyansh7924 <guptadivyansh49@gmail.com>
2019-03-30 02:13:14 +0530

    landing page proto stage1 init

--- src/index.js
+++ src/index.js
@@ -1,12 +1,53 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import './App.css';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+var style = {
+    backgroundColor: 'orange',
+    color: 'white',
+    fontFamily: 'Arial'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+let message = {
+    title: "EventsIgnited",
+    minutes: 9
+}
+
+class Message extends React.Component{
+    render(){
+        return(
+            <div className="App-landing">
+                <nav class="navbar">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <a class="navbar-brand" href="#">{this.props.title}</a>
+                        </div>
+                        <div className="App-header-elements">
+                        <ul class="nav navbar-nav">
+                                <li class="active"><a href="#">Portfolio</a></li>
+                                <li><a href="#">Login</a></li>
+                                <li><a href="#">Signup</a></li>
+                                <li><a href="#">Help</a></li>
+                            </ul>
+                            <button class="btn btn-danger navbar-btn">How it Works</button>
+                        </div>
+                    </div>
+                </nav>
+                <div className="App-landing-content">
+                    <h2>
+                        Hi, We organise Great Events
+                    </h2>
+                    <img src="ill1.svg" className="illustration"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+
+ReactDOM.render(
+    <Message
+    title={message.title}
+    minutes={message.minutes}/>,
+    document.getElementById('root')   
+)
