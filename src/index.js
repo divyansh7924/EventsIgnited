912cf6ac34598f4c8e89e71c307927fb31282545
divyansh7924 <guptadivyansh49@gmail.com>
2019-03-30 16:50:15 +0530

    Categories shown on landing page

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import './App.css';
+import brokerage from './assets/images/brokerage.png'
+import theater from './assets/images/theater.png'
+import donation from './assets/images/donation.png'
+import healthy from './assets/images/healthy.png'
+import holiday from './assets/images/sunbed.png'
 
 let message = {
     title: "EventsIgnited",
@@ -32,7 +37,7 @@ class Message extends React.Component{
                         Hi, We organise Great Events
                     </h2>
                     <button class="btn btn-primary navbar-btn">Know more</button>
-                    <img src="ill1.svg" className="illustration"/>
+                    <img className="illustration"/>
                 </div>
                 <div className="features">
                     <div className="row">
@@ -47,7 +52,27 @@ class Message extends React.Component{
                         </div>
                     </div>
                 </div>
-                
+                <div className="categories">
+                        <div className="categories-column-1 row">
+                            <div class="vert-move-delay circle">
+                                <img src={brokerage} className="illustration-categories"/>
+                            </div>
+                            <div className="vert-move-delays circle">
+                                <img src={theater} className="illustration-categories"/>
+                            </div>
+                            <div className="vert-move-delay circle end">
+                                <img src={donation} className="illustration-categories"/>
+                            </div>
+                        </div>
+                        <div className="categories-column-2 row">
+                            <div class="vert-move-delay circle">
+                                <img src={healthy} className="illustration-categories"/>
+                            </div>
+                            <div className="vert-move-delays circle">
+                                <img src={holiday} className="illustration-categories"/>
+                            </div>
+                        </div>
+                </div>
             </div>
         )
     }
