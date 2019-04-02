4a7034e56da0929fc1fc6a2a40e1e1a014c712b6
disolaterx <kartikyadav@protonmail.com>
2019-04-02 17:43:50 +0530

    Updates

---
+++ precache-manifest.cc6c51ab3ae9c550699574647ecc63ed.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = [
+  {
+    "revision": "0a24f4e5666df939524f8f8dcaf367af",
+    "url": "/EventsIgnited/static/media/art-cat.0a24f4e5.jpg"
+  },
+  {
+    "revision": "752c8cb95b6350515397",
+    "url": "/EventsIgnited/static/css/main.aac0edb8.chunk.css"
+  },
+  {
+    "revision": "5b7ed6f816e1388d15ec",
+    "url": "/EventsIgnited/static/js/runtime~main.7fe71451.js"
+  },
+  {
+    "revision": "257a426da6566e1a37e0",
+    "url": "/EventsIgnited/static/js/2.4f725856.chunk.js"
+  },
+  {
+    "revision": "7a7681beb6783dc4802be6ac14d24d5a",
+    "url": "/EventsIgnited/static/media/aboutuspic1.7a7681be.jpg"
+  },
+  {
+    "revision": "ef3ee0965fcb54324c35a05f5bda31b0",
+    "url": "/EventsIgnited/static/media/aboutuspic2.ef3ee096.jpg"
+  },
+  {
+    "revision": "d55a4c8214dc484b4be8faf2a140cd35",
+    "url": "/EventsIgnited/static/media/aboutuspic3.d55a4c82.jpg"
+  },
+  {
+    "revision": "79ed25f03338c6d5df50e810aa1c0601",
+    "url": "/EventsIgnited/static/media/brainstorming.79ed25f0.svg"
+  },
+  {
+    "revision": "3ee07e883affdc5b858e393764b141aa",
+    "url": "/EventsIgnited/static/media/mobile_2.3ee07e88.svg"
+  },
+  {
+    "revision": "39ba9c38035fc3670d0fe2d955341b79",
+    "url": "/EventsIgnited/static/media/Explore.39ba9c38.svg"
+  },
+  {
+    "revision": "4fd59bef088d7f15eb6ceecfacf85fe6",
+    "url": "/EventsIgnited/static/media/organise.4fd59bef.svg"
+  },
+  {
+    "revision": "dfa5851381486e2649487bd749d36426",
+    "url": "/EventsIgnited/static/media/plans.dfa58513.svg"
+  },
+  {
+    "revision": "8a5e1098f4bdc34fa363ca8722897b43",
+    "url": "/EventsIgnited/static/media/music-cat.8a5e1098.jpg"
+  },
+  {
+    "revision": "752c8cb95b6350515397",
+    "url": "/EventsIgnited/static/js/main.2a1fa3bb.chunk.js"
+  },
+  {
+    "revision": "c60d7907757b8ff4fed26f0d1b3327b0",
+    "url": "/EventsIgnited/static/media/business-cat.c60d7907.jpg"
+  },
+  {
+    "revision": "b8703e7d224a528456c7527250cd0d6d",
+    "url": "/EventsIgnited/static/media/parties-cat.b8703e7d.jpg"
+  },
+  {
+    "revision": "7535f09348646d2b073d4adc7d7baefe",
+    "url": "/EventsIgnited/static/media/classes-cat.7535f093.jpg"
+  },
+  {
+    "revision": "56fc76f967e6bf43ce653d14f95401d4",
+    "url": "/EventsIgnited/static/media/sports-cat.56fc76f9.jpg"
+  },
+  {
+    "revision": "7b4f1295cd025978ab1a26d3b7057d91",
+    "url": "/EventsIgnited/static/media/bg.7b4f1295.svg"
+  },
+  {
+    "revision": "370e9b212844af7f365b22811c0e2b8a",
+    "url": "/EventsIgnited/static/media/brokerage.370e9b21.png"
+  },
+  {
+    "revision": "0983738e8636006fcad2731d1c62e557",
+    "url": "/EventsIgnited/static/media/theater.0983738e.png"
+  },
+  {
+    "revision": "275e3c2d9cfb2d7a5d62c3fe44d3923b",
+    "url": "/EventsIgnited/static/media/donation.275e3c2d.png"
+  },
+  {
+    "revision": "0a4c53f147f26a3a11e5deee366542db",
+    "url": "/EventsIgnited/static/media/healthy.0a4c53f1.png"
+  },
+  {
+    "revision": "13fbf8e090ec57ef24aa629e8f2ce053",
+    "url": "/EventsIgnited/static/media/sunbed.13fbf8e0.png"
+  },
+  {
+    "revision": "9f8310ea3576d601ac49ebc3215c7a12",
+    "url": "/EventsIgnited/static/media/underline.9f8310ea.svg"
+  },
+  {
+    "revision": "d9416d60db27efc256b0c6ea1c61f3eb",
+    "url": "/EventsIgnited/index.html"
+  }
+];
